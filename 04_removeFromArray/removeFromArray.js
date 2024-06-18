7a57a583346a158518d663aeeb3951b5773f5f27
Chris Skeens <cskee004@odu.edu>
2024-06-18 15:25:24 -0400

    Test 1 2 3 pass.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,11 @@
 const removeFromArray = function(contactArray, ...contactToDelete) {
     let contactsCopyArray = [...contactArray]
     
-    // debugger;
+    
 
     for (toDelete of contactToDelete) {
         let pos = contactsCopyArray.indexOf(toDelete)
         while (contactsCopyArray.includes(toDelete)) {
-            // let pos = contactArray.indexOf(toDelete)
             contactsCopyArray.splice(pos, 1)
             if (contactsCopyArray.length === 1 && contactsCopyArray.includes(toDelete)) {
                 contactsCopyArray.splice(-1, 1)
@@ -15,9 +14,8 @@ const removeFromArray = function(contactArray, ...contactToDelete) {
         }
     
         continue;
+    }
     
-}
-
     return contactsCopyArray;
 };
 
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    let sum = 0
+
+    if (Math.sign(start) === -1 || Math.sign(end) === -1) {
+        return 'ERROR';
+    }
+    else {
+        for (let i = 0; i < Math.abs(start + end); ++i) {
+            sum += i
+        }
+    }
+    
+    return sum
 };
 
 // Do not edit below this line
