aaabb574c29e54ce08e2dffde55503f16bf8534f
Chris Skeens <cskee004@odu.edu>
2024-06-18 12:12:44 -0400

    Test 7 pass.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,12 @@
 const removeFromArray = function(contactArray, ...contactToDelete) {
     let contactsCopyArray = [...contactArray]
     
-    for (toDelete of contactToDelete) {
-        console.log("Size of contact copy array: " + contactsCopyArray.length)
-        
+    debugger;
 
+    for (toDelete of contactToDelete) {
+        let pos = contactsCopyArray.indexOf(toDelete)
         while (contactsCopyArray.includes(toDelete)) {
-            let pos = contactArray.indexOf(toDelete)
+            // let pos = contactArray.indexOf(toDelete)
             contactsCopyArray.splice(pos, 1)
             if (contactsCopyArray.length === 1 && contactsCopyArray.includes(toDelete)) {
                 contactsCopyArray.splice(-1, 1)
