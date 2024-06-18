d3eb49d44a260749701a9f88f2efd5fb278764dd
Chris Skeens <cskee004@odu.edu>
2024-06-18 16:09:53 -0400

    Test 1 2 3 4 5 6 pass.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farTemp) {
+  
+  let celTemp = (farTemp - 32) * (5/9)
+  return Number(celTemp.toFixed(1))
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celTemp) {
+  
+  let farTemp = (celTemp * (9/5) + 32)
+  return Number(farTemp.toFixed(1))
 };
 
 // Do not edit below this line
